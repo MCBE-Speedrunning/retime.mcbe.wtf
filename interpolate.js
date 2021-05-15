99f7ceddcd2ce99a8e2439b3a04e5a3ba236c64a
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-15 17:58:10 +0000

    update interpolate.js

--- interpolate.js
+++ interpolate.js
@@ -3,11 +3,11 @@ const regex = /\${[^{]+}/g;
 export default function interpolate(template, variables, fallback) {
     return template.replace(regex, (match) => {
         const path = match.slice(2, -1).trim();
-        return getObjPath(path, variables, fallback);
+        return getObjPath(path, variables);
     });
 }
 
 //get the specified property or nested property of an object
-function getObjPath(path, obj, fallback = '') {
-    return path.split('.').reduce((res, key) => res[key] || fallback, obj);
+function getObjPath(path, obj) {
+    return path.split('.').reduce((res, key) => res[key], obj);
 }
