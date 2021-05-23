fea67dbe1516fbf765de9a45631684979aeb0a3f
Mango0x45 <thomasvoss@live.com>
2021-05-23 15:11:02 +0200

    If no value given use the default mod message

--- settings.js
+++ settings.js
@@ -86,7 +86,7 @@ function set_mod_message(event)
 {
 	if (event && event.target.value.replace(/\s/g, ""))
 		localStorage.setItem("custom_mod_message", event.target.value);
-	else if (!localStorage.getItem("custom_mod_message")) {
+	else {
 		if (event)
 			event.target.value = mod_message_default;
 		localStorage.setItem("custom_mod_message", mod_message_default);
